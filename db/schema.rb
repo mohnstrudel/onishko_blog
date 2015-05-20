16c0bfafd5fef891752710b45dd72b7644ab604e
Anton Kostin <schnibba@gmail.com>
2015-05-20 22:16:40 +0300

    Add category_id as column, add new post creation with category attached

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150516122844) do
+ActiveRecord::Schema.define(version: 20150520191141) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "photos", force: :cascade do |t|
     t.string   "title"
@@ -24,8 +30,9 @@ ActiveRecord::Schema.define(version: 20150516122844) do
     t.string   "title"
     t.text     "body"
     t.boolean  "published"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "category_id"
   end
 
 end
