d9930eb7acda34982dc19c7600c7a4e9d3bb3a75
Anton Kostin <schnibba@gmail.com>
2015-05-23 18:56:23 +0300

    commit before branch switch

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :find_post, only: [:edit, :update, :show, :destroy]
 
   def index
-  	@posts = Post.all
+  	@posts = Post.all.sort_by("created_at DESC")
   end
 
   def edit
