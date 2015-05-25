cb6c2e45cf224186140795e792915bf0ac356cc1
Anton Kostin <schnibba@gmail.com>
2015-05-25 23:52:44 +0300

    add comment user association and user email output to comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150525184401) do
+ActiveRecord::Schema.define(version: 20150525201259) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20150525184401) do
     t.integer  "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "photos", force: :cascade do |t|
